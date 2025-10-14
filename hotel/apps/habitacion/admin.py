350a815682cb4f72e61a763ffeab7070dfafe37a
Nadir <nadiradad@gmail.com>
2025-10-14 19:43:15 -0300

    Agregando habitaciones

--- hotel/apps/habitacion/admin.py
+++ hotel/apps/habitacion/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+class TipoHabitacionAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'descripcion')
+    search_fields = ('nombre',)
+
+class HabitacionAdmin(admin.ModelAdmin):
+    list_display = ('numero', 'tipo', 'precio', 'disponible')
+    list_filter = ('tipo', 'disponible')
+    search_fields = ('numero',)
--- hotel/apps/habitacion/models.py
+++ hotel/apps/habitacion/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class TipoHabitacion(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+
+class Habitacion(models.Model):
+    numero = models.CharField(max_length=10, unique=True)
+    tipo = models.ForeignKey(TipoHabitacion, on_delete=models.CASCADE)
+    precio = models.DecimalField(max_digits=8, decimal_places=2)
+    disponible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'Habitación {self.numero} - {self.tipo.nombre}'

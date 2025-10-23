79528b5954103fada7bab6fce6469c4b7c45a738
Gaston Ariel Perrone <gastonarielp1812@gmail.com>
2025-10-23 17:10:36 -0300

    Eliminación de BBDD y nuevas migraciones para no sobreescribir migraciones. Foreignkey id_usuario aplicada en models.py de reserva.

--- hotel/apps/reserva/models.py
+++ hotel/apps/reserva/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Reserva(models.Model):
-    id_habitacion = models. ForeignKey('habitacion.Habitacion', on_delete=models.CASCADE)
+    id_habitacion = models.ForeignKey('habitacion.Habitacion', on_delete=models.CASCADE)
+    id_usuario = models.ForeignKey('usuario.Usuarios', on_delete=models.CASCADE)
     id_servicio_adicional = models.ForeignKey('servicio_adicional.Servicio_adicional', on_delete=models.CASCADE)
     estado = models.CharField(max_length=50)
     monto_total = models.DecimalField(max_digits=8, decimal_places=2)
 
     def __str__(self):
-        return f'Reserva {self.id} - {self.estado}' # type: ignore
+        return f'Reserva {self.id}' # type: ignore
